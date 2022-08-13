812a79d104caeaf8461c1ce999042740de8e835b
Aryav <aryavbhola@gmail.com>
2022-08-13 23:18:35 +0530

    Home Events Section - Home Page Complete

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,6 +3,7 @@ import FrontSection from "./FrontSection";
 import WWDSection from "./WWDSection";
 import EFPSection from "./EFPSection";
 import ContactsSection from "./ContactsSection";
+import HomeEventsSection from "./HomeEventsSection";
 import Footer from "../../Components/Footer/Footer";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <FrontSection content="At GDSC NSUT, we aim to learn, teach and grow. Together. The place where creative minds come together to build something amazing." />
       <WWDSection />
       <EFPSection />
+      <HomeEventsSection />
       <ContactsSection />
       <Footer />
     </>
