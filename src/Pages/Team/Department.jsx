90db9a53257060aed709ed8145607a90fb306427
Aryav <aryavbhola@gmail.com>
2022-08-20 18:26:11 +0530

    Department Card Changes+ Scroll position fixed

--- src/Pages/Team/Department.jsx
+++ src/Pages/Team/Department.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { useEffect } from 'react';
 import "./team.css";
 import { useParams, Navigate } from 'react-router-dom';
 import Departments from "./Departments.json";
@@ -7,6 +8,19 @@ import DepartmentUtility from "./DepartmentUtility";
 
 export default function Department() {
   const { department } = useParams();
+
+  // For Scroll Behaviour Smmoth
+  // useEffect(() => {
+  //   window.scrollTo({
+  //     top: 0,
+  //     left: 0,
+  //     behavior: 'smooth'
+  //   });
+  // }, [])
+
+  // For Scroll Behaviour Auto 
+  // Intentionally not using useEffect here so that it is scrolled to top before component gets mounted
+  window.scrollTo(0, 0);
 
   if (department === "all-departments") {
     return (
