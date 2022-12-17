517e337ea6d27a7220cb345dad8d3eed3425d462
Naman Dureja <durejanaman29@gmail.com>
2022-12-17 23:09:43 +0530

    fixed

--- src/Pages/Events/card.jsx
+++ src/Pages/Events/card.jsx
@@ -1,46 +1,34 @@
-import React from "react";
-import styles from "./events.module.css";
-import image from "../../Assets/Images/hello.png";
-import bg from "../../Assets/Images/bg.png";
-// import { IoLocation } from "react-icons/io";
-import { MdLocationPin } from "react-icons/md";
-// import {IoTicket} from 'react-icons/io';
-import { AiTwotoneCalendar } from "react-icons/ai";
+import React from 'react'
+import { FaMapMarkerAlt } from "react-icons/fa";
 
-const Card = ({ date, venue, name }) => {
+const Card = ({ event, showEventModal }) => {
+
+  const { name, date, description, venue, image } = event;
+  const Image = require(`../../Assets/Images/${image}`);
+
+  function handleClick() {
+    showEventModal(event);
+  }
+
   return (
-    <div className={styles.container}>
-      <div>
-        <img src={image} alt="" />
+    <div className='event-card'>
+      <div className='event-image-container'>
+        <img src={Image} alt={name} loading="lazy" />
       </div>
-      <div>
-        <div className={styles.content}>
-          <div className={styles.heading}>{name}</div>
-          {/* <div className={styles.date}>18 August 2022</div> */}
-          <div className={styles.location}>
-            <div className={styles.locationIcon}>
-              <MdLocationPin className={styles.pin} size={20} />
-            </div>
-            <div className={styles.locationContent}>{venue}</div>
+      <div className='event-info-container'>
+        <h2 className='event-name'>{name}</h2>
+        <h5 className='event-date'>{date}</h5>
+        <p className='event-desc'>{description}</p>
+        <div className='event-venue-wrapper'>
+          <div className='event-venue-icon'>
+            <FaMapMarkerAlt />
           </div>
-          <div className={styles.ticket}>
-            <div className={styles.ticketIcon}>
-              <AiTwotoneCalendar className={styles.calendar} size={20} />
-            </div>
-            <div className={styles.ticketContent}>{date}</div>
-          </div>
-          <div className={styles.buttonContainer}>
-            <button className={styles.button1}>
-              <div className={styles.buttonText}>Join</div>
-            </button>
-            <button className={styles.button2}>
-              <div className={styles.buttonText2}>Details</div>
-            </button>
-          </div>
+          <span className='event-venue'>{venue}</span>
         </div>
+        <button className='event-learn-more-btn' onClick={handleClick}>Learn More</button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
--- src/Pages/Events/index.jsx
+++ src/Pages/Events/index.jsx
@@ -1,75 +1,150 @@
-import React from "react";
-import Card from "./card";
-import styles from "./events.module.css";
+import React, { useEffect, useRef } from "react";
+import Card from "./Card";
+import "./events.css";
 import { useState } from "react";
-import eve from "./events_upcoming.json";
-import pastEve from "./events_past.json";
+import upcomingEvents from "./events_upcoming.json";
+import pastEvents from "./events_upcoming.json";
+import { MdArrowBackIosNew, MdFilterList } from "react-icons/md";
+import { RiSearchLine } from "react-icons/ri";
+import { IoIosArrowDown } from "react-icons/io";
+import { HiLocationMarker } from "react-icons/hi";
+import Carousel from "./Carousel";
+
 const Events = () => {
   const [events, setEvents] = useState("U");
-  const onClickUpcomingEventHandler = (e) => {
-    e.preventDefault();
-    setEvents("U");
+  const [modalEvent, setModalEvent] = useState({});
+  const modalOverlayRef = useRef();
+  const modalRef = useRef();
+
+  function showEventModal(event) {
+    modalOverlayRef.current.classList.add("show");
+    setModalEvent(event);
   }
-  const onClickPastEventHandler = (e) => {
-    e.preventDefault();
-    setEvents("P");
+
+  function hideEventModal(e) {
+    modalOverlayRef.current.classList.remove("show");
   }
+
+  function handleShowDropdown(e) {
+    const isDropdownButton = e.target.matches(".dropdown-btn");
+    if (!isDropdownButton && e.target.closest(".dropdown") != null) return;
+
+    let currentDropdown;
+    if (isDropdownButton) {
+      currentDropdown = e.target.closest(".dropdown");
+      currentDropdown.classList.add("show");
+    }
+
+    document.querySelectorAll(".dropdown.show").forEach(dropdown => {
+      if (dropdown === currentDropdown) return;
+      dropdown.classList.remove("show");
+    })
+  }
+
+  useEffect(() => {
+    // toggle dropdown
+    document.addEventListener("click", handleShowDropdown);
+    return () => document.removeEventListener("click", handleShowDropdown);
+  }, [])
+
   return (
-    <div>
-      <h1 className={styles.super}>Events and Workshops</h1>
-      <div className={styles.eventHeading}>
-        <h2 className={(events === "U") ? styles.eventChange : ""} onClick={onClickUpcomingEventHandler} >Upcoming Events</h2>
-        <h2>|</h2>
-        <h2 className={(events === "P") ? styles.eventChange : ""} onClick={onClickPastEventHandler} >Past Events</h2>
+    <>
+      <main>
+        <div className={'carousel-container'}>
+          <Carousel />
+        </div>
+        <div className={'search-container'}>
+          <div className="search-wrapper">
+            <input type="text" className="search-input" placeholder="Search" />
+            <div className="search-icon">
+              <RiSearchLine />
+            </div>
+          </div>
+          <div className="dropdown">
+            <button className="dropdown-btn" onFocus={handleShowDropdown}>
+              <div className="category-icon">
+                <MdFilterList />
+              </div>
+              <span>Categories</span>
+              <div className="dropdown-toggle">
+                <IoIosArrowDown />
+              </div>
+            </button>
+            <div className="dropdown-menu">
+              <div className="dropdown-item" onClick={() => setEvents("P")}>Past Events</div>
+              <div className="dropdown-item" onClick={() => setEvents("U")}>Upcoming Events</div>
+            </div>
+          </div>
+        </div>
+        <div className={'events-container'}>
+          <div className={'events-header'}>
+            <h1>{events === "U" ? "Upcoming" : "Past"} Events</h1>
+            <span>View All</span>
+          </div>
+          <div className="event-cards">
+            {
+              events === "U" ?
+              upcomingEvents.map(event => {
+                return <Card key={event.id} event={event} showEventModal={showEventModal} />
+              }) :
+              pastEvents.map(event => {
+                return <Card key={event.id} event={event} showEventModal={showEventModal} />
+              })
+            }
+          </div>
+        </div>
+      </main>
+      <div ref={modalOverlayRef} className="modal-overlay">
+        <div ref={modalRef} className="modal" tabIndex={-1}>
+          {
+            Object.keys(modalEvent).length > 0 &&
+            <>
+              <div className="event-cover-image-container">
+                <img className="event-cover-image" src={require(`../../Assets/Images/${modalEvent.coverImage}`)} alt={modalEvent.name} />
+                <div className="modal-close" onClick={hideEventModal}>
+                  <MdArrowBackIosNew />
+                </div>
+              </div>
+              <div className="event-content-container">
+                <div className="event-content-header">
+                  <div>
+                    <h2>{modalEvent.name}</h2>
+                    <p style={{ fontSize: "1.4rem" }}>Organized By {modalEvent.organizer}</p>
+                  </div>
+                  <div className="event-register">
+                    <button className="register-btn">Register</button>
+                    <p>Registration ends on</p>
+                    <p>{modalEvent.registrationEndDate}</p>
+                  </div>
+                </div>
+                <div className="event-content-about">
+                  <h3>About</h3>
+                  <p>{modalEvent.description}</p>
+                </div>
+                <div className="event-content-info">
+                  {
+                    (modalEvent.info ?? []).map((info, index) => {
+                      return (
+                        <div key={index}>
+                          <h3>{info.title}</h3>
+                          {
+                            info.title?.toLowerCase() === "location" ?
+                              <p className="event-location"><HiLocationMarker /> {info.description}</p> :
+                              info.title?.toLowerCase() === "contact" ?
+                                <p className="event-contact">{info.description}</p> :
+                                <p>{info.description}</p>
+                          }
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </>
+          }
+        </div>
       </div>
-      {events === "U" && (
-        <div className={styles.eventsContainer}>
-          {eve.map((even) => {
-            return (
-              <>
-                <Card
-                  key={even.id}
-                  date={even.date}
-                  venue={even.venue}
-                  name={even.name}
-                />
-              </>
-            );
-          })}
-
-          {/* <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card /> */}
-        </div>
-      )}
-      {events === "P" && (
-        <div className={styles.eventsContainer}>
-          {pastEve.map((even) => {
-            return (
-              <>
-                <Card
-                  key={even.id}
-                  date={even.date}
-                  venue={even.venue}
-                  name={even.name}
-                />
-              </>
-            );
-          })}
-
-          {/* <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card /> */}
-        </div>
-      )}
-
-    </div>
+    </>
   );
 };
 
